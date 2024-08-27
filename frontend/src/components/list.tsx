88bfe296d6c403e9dfa46c7a1c57e88ec9667e83
Edward Shin <shinedwardc@gmail.com>
2024-08-27 17:20:18 +0900

    Search functionality
    Basic search functionality for now (include string function)

--- frontend/src/components/list.tsx
+++ frontend/src/components/list.tsx
@@ -11,6 +11,9 @@ import axios from "axios";
 const List = () => {
   const [data, setData] = useState<ExpenseInterface[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [search, setSearch] = useState("");
+  const [useFilteredData, setUseFilteredData] = useState(false);
+  const [filteredData, setFilteredData] = useState<ExpenseInterface[]>([]);
 
   const fetchExpenses = async () => {
     try {
@@ -52,6 +55,21 @@ const List = () => {
     }
   };
 
+  const handleSearch = (e) => {
+    const query = e.target.value;
+    setSearch(query);
+    let searchedData = data;
+    if (query.length > 0){
+      setUseFilteredData(true);
+      const lowerCaseQuery = query.toLowerCase();
+      searchedData = searchedData.filter((expense) => expense.description.toLowerCase().includes(lowerCaseQuery) || expense.category.name.toLowerCase().includes(lowerCaseQuery))
+      setFilteredData(searchedData);
+    }
+    else{
+      setUseFilteredData(false);
+    }
+  }
+
   return (
     <>
       {/*<div>
@@ -60,6 +78,21 @@ const List = () => {
       {!isLoading ? (
         data.length > 0 ? (
           <div className="mb-6 border-cyan-500 overflow-x-auto">
+            <div className="mb-4 flex justify-center">
+              <label className="input input-bordered flex items-center w-64 gap-2">
+                <input type="text" className="grow" placeholder="Search" value={search} onChange={handleSearch}/>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="h-4 w-4 opacity-70">
+                  <path
+                    fillRule="evenodd"
+                    d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                    clipRule="evenodd" />
+                </svg>
+              </label>
+            </div>
             <table className="table table-auto border-collapse border border-slate-400 text-sm text-center">
               <thead>
                 <tr>
@@ -74,7 +107,7 @@ const List = () => {
                 </tr>
               </thead>
               <tbody>
-                {data.map((expense, index) => (
+                {(useFilteredData ? filteredData : data).map((expense, index) => (
                   <Expense
                     key={index}
                     data={expense}
